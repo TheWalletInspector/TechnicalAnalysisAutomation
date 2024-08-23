5687fcc4d0271ed367058a8ecd1c4f1dd09bba32
Luke Taranto <luke.taranto@gmail.com>
2024-08-23 23:50:45 +1000

    add local maxima function and tests

--- technical_analysis_automation/swing_chart_candle_plotter.py
+++ technical_analysis_automation/swing_chart_candle_plotter.py
@@ -6,61 +6,49 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 
-def is_data_point_a_local_swing(data_set: np.array, current_index: int, time_radius: int, is_top: bool) -> bool:
-    """
-    Check if the current index is a local swing based on the data set and time radius.
-    """
-    current_index = current_index - 1
+def _is_local_extreme(data_set: np.array, current_index: int, time_radius: int, is_top: bool) -> bool:
     if current_index < time_radius:
         print("")
-        print(f"current_index: {current_index} is less than time_radius * 2 + 1: {time_radius * 2 + 1}")
+        print(f"current_index: {current_index} is less than time_radius: {time_radius}")
         logging.info(f"current_index: {current_index} does not have enough data points to form a rolling window")
         return False
 
-    comparison_operator = np.greater if is_top else np.less
+    start = max(0, current_index - time_radius)
+    end = min(len(data_set), current_index + time_radius + 1)
+    window = data_set[start:end]
 
-    time_span_starting_index = current_index - time_radius
-    time_span_starting_value = data_set[time_span_starting_index]
+    comparison_operator = np.max if is_top else np.min
+    is_extreme = data_set[current_index] == comparison_operator(window) and np.sum(
+        window == data_set[current_index]) == 1
 
-    print(f"current_index: {current_index}")
-    print(f"time_span_starting_index: {time_span_starting_index}")
-    print(f"time_span_starting_value: {time_span_starting_value}")
-
-    # If any of the data points within the time span are greater than the starting value, return False (not a local extreme)
-    for loop_index in range(1, time_radius + 1):
-        condition1 = not comparison_operator(data_set[time_span_starting_index + loop_index], time_span_starting_value)
-        print(f"data_set[time_span_starting_index + loop_index]: {data_set[time_span_starting_index + loop_index], time_span_starting_value}")
-
-        condition2 = not comparison_operator(data_set[time_span_starting_index - loop_index], time_span_starting_value)
-        print(f"data_set[time_span_starting_index + loop_index]: {data_set[time_span_starting_index + loop_index], time_span_starting_value}")
-
-        print(f"loop_index: {loop_index}, condition1: {condition1}, condition2: {condition2}")
-
-
-    return all(
-        not comparison_operator(data_set[time_span_starting_index + loop_index], time_span_starting_value) and
-        not comparison_operator(data_set[time_span_starting_index - loop_index], time_span_starting_value)
-        for loop_index in range(1, time_radius + 1)
-    )
+    return bool(is_extreme)
 
 
 def detect_swing_extremes_across_data_set(data_set: np.array, time_radius: int) -> (pd.DataFrame, pd.DataFrame):
+    """
+    Detects swing highs and lows across the data set
+    """
     local_tops = [
-        [index_of_bar_to_test, index_of_bar_to_test - time_radius, data_set[index_of_bar_to_test - time_radius]]
+        [index_of_bar_to_test, data_set[index_of_bar_to_test]]
         for index_of_bar_to_test in range(len(data_set))
-        if is_data_point_a_local_swing(data_set=data_set, current_index=index_of_bar_to_test, time_radius=time_radius,
-                                       is_top=True)
+        if _is_local_extreme(data_set=data_set,
+                             current_index=index_of_bar_to_test,
+                             time_radius=time_radius,
+                             is_top=True)
     ]
 
     local_bottoms = [
-        [loop_index, loop_index - time_radius, data_set[loop_index - time_radius]]
-        for loop_index in range(len(data_set))
-        if is_data_point_a_local_swing(data_set, loop_index, time_radius, False)
+        [index_of_bar_to_test, data_set[index_of_bar_to_test]]
+        for index_of_bar_to_test in range(len(data_set))
+        if _is_local_extreme(data_set=data_set,
+                             current_index=index_of_bar_to_test,
+                             time_radius=time_radius,
+                             is_top=False)
     ]
 
     return (
-        pd.DataFrame(local_tops, columns=["confirmation_index", "index_of_swing", "price_of_swing"]),
-        pd.DataFrame(local_bottoms, columns=["confirmation_index", "index_of_swing", "price_of_swing"])
+        pd.DataFrame(local_tops, columns=["index_of_swing", "price_of_swing"]),
+        pd.DataFrame(local_bottoms, columns=["index_of_swing", "price_of_swing"])
     )
 
 
@@ -69,36 +57,41 @@ def main() -> None:
     data['date'] = data['date'].astype('datetime64[s]')
     data = data.set_index('date')
 
-    swing_tops, swing_bottoms = detect_swing_extremes_across_data_set(data['close'].to_numpy(), 10)
+    swing_tops, swing_bottoms = detect_swing_extremes_across_data_set(data['close'].to_numpy(), 2)
 
-    # Create new series for the swing highs and lows
-    data['swing_high'] = np.nan
-    data['swing_low'] = np.nan
-    data.loc[swing_tops['index_of_swing'], 'swing_high'] = swing_tops['price_of_swing']
-    data.loc[swing_bottoms['index_of_swing'], 'swing_low'] = swing_bottoms['price_of_swing']
+    print("Swing Tops")
+    print(swing_tops)
 
-    # Plotting as a trading bar chart
-    fig, axlist = mpf.plot(data,
-                           type='bar',  # candlestick chart
-                           style='yahoo',  # chart style
-                           addplot=[
-                               mpf.make_addplot(data['swing_high'], color='green', linestyle='solid'),
-                               mpf.make_addplot(data['swing_low'], color='red', linestyle='solid'),
-                               mpf.make_addplot(swing_tops['price_of_swing'], color='green', marker='^', markersize=8),
-                               mpf.make_addplot(swing_bottoms['price_of_swing'], color='red', marker='v', markersize=8)
-                           ],
-                           volume=False,
-                           figratio=(10, 6),  # figure size
-                           title='BTCUSDT Trading Chart',
-                           ylabel='Price',
-                           ylabel_lower='Volume'
-                           )
-
-    # Display legend
-    axlist[0].legend()
-
-    # Show the plot
-    plt.show()
+    print("Swing Bottoms")
+    print(swing_bottoms)
+    # # Create new series for the swing highs and lows
+    # data['swing_high'] = np.nan
+    # data['swing_low'] = np.nan
+    # data.loc[swing_tops['index_of_swing'], 'swing_high'] = swing_tops['price_of_swing']
+    # data.loc[swing_bottoms['index_of_swing'], 'swing_low'] = swing_bottoms['price_of_swing']
+    #
+    # # Plotting as a trading bar chart
+    # fig, axlist = mpf.plot(data,
+    #                        type='bar',  # candlestick chart
+    #                        style='yahoo',  # chart style
+    #                        addplot=[
+    #                            mpf.make_addplot(data['swing_high'], color='green', linestyle='solid'),
+    #                            mpf.make_addplot(data['swing_low'], color='red', linestyle='solid'),
+    #                            mpf.make_addplot(swing_tops['price_of_swing'], color='green', marker='^', markersize=8),
+    #                            mpf.make_addplot(swing_bottoms['price_of_swing'], color='red', marker='v', markersize=8)
+    #                        ],
+    #                        volume=False,
+    #                        figratio=(10, 6),  # figure size
+    #                        title='BTCUSDT Trading Chart',
+    #                        ylabel='Price',
+    #                        ylabel_lower='Volume'
+    #                        )
+    #
+    # # Display legend
+    # axlist[0].legend()
+    #
+    # # Show the plot
+    # plt.show()
 
 
 if __name__ == '__main__':
--- tests/test__swing_chart_candle_plotter.py
+++ tests/test__swing_chart_candle_plotter.py
@@ -1,52 +1,89 @@
 import numpy as np
 
-from technical_analysis_automation.swing_chart_candle_plotter import is_data_point_a_local_swing
+from technical_analysis_automation.swing_chart_candle_plotter import _is_local_extreme
 
 
 class TestSwingChartCandlePlotter:
     def test__is_data_point_a_local_swing__should_not_have_enough_data_points(self) -> None:
-        result = is_data_point_a_local_swing(data_set=np.array([1, 2, 3, 4, 5, 4, 3, 2, 1]),
-                                             current_index=4,
-                                             time_radius=2,
-                                             is_top=True)
+        result = _is_local_extreme(data_set=np.array([1, 2, 3, 4, 5, 4, 3, 2, 1]),
+                                   current_index=1,
+                                   time_radius=2,
+                                   is_top=True)
         assert result is False
 
     def test__is_data_point_a_local_swing__should_be_a_local_swing_top(self) -> None:
-        result = is_data_point_a_local_swing(data_set=np.array([1, 2, 3, 4, 5, 4, 3, 2, 1]),
-                                             current_index=5,
-                                             time_radius=1,
-                                             is_top=True)
+        result = _is_local_extreme(data_set=np.array([1, 2, 3, 4, 5, 4, 3, 2, 1]),
+                                   current_index=4,
+                                   time_radius=1,
+                                   is_top=True)
         assert result is True
 
     def test__is_data_point_a_local_swing__should_be_a_local_2d_wide_swing_top(self) -> None:
-        result = is_data_point_a_local_swing(data_set=np.array([1, 2, 3, 4, 5, 4, 3, 2, 1]),
-                                             current_index=5,
-                                             time_radius=2,
-                                             is_top=True)
+        result = _is_local_extreme(data_set=np.array([1, 2, 3, 4, 5, 4, 3, 2, 1]),
+                                   current_index=4,
+                                   time_radius=2,
+                                   is_top=True)
         assert result is True
 
-    # not np.greater(data_set[3 + 1], 4) and
-    # not np.greater(data_set[3 - 1], 4)
-    # for loop_index in range(1, 2 + 1)
+    def test__is_data_point_a_local_swing__should_not_be_a_local_2d_wide_swing_top(self) -> None:
+        data_set = np.array([1, 2, 5, 4, 5, 4, 3, 2, 1])
 
-
-    def test__is_data_point_a_local_swing__should_not_be_a_local_swing_top(self) -> None:
-        result = is_data_point_a_local_swing(data_set=np.array([1, 2, 3, 4, 5, 4, 3, 2, 1]),
-                                             current_index=6,
+        result_first_max = _is_local_extreme(data_set=data_set,
+                                             current_index=2,
                                              time_radius=2,
                                              is_top=True)
+
+        result_second_mox = _is_local_extreme(data_set=data_set,
+                                              current_index=4,
+                                              time_radius=2,
+                                              is_top=True)
+
+        assert result_first_max is False
+        assert result_second_mox is False
+
+    def test__is_data_point_a_local_swing__should_not_be_a_local_swing_top(self) -> None:
+        result = _is_local_extreme(data_set=np.array([1, 2, 5, 4, 5, 4, 3, 2, 1]),
+                                   current_index=4,
+                                   time_radius=2,
+                                   is_top=True)
         assert result is False
 
     def test__is_data_point_a_local_swing__should_not_be_a_local_swing_bottom(self) -> None:
-        result = is_data_point_a_local_swing(data_set=np.array([1, 2, 3, 4, 5, 4, 3, 2, 1]),
-                                             current_index=5,
-                                             time_radius=1,
-                                             is_top=False)
-        assert result is False
+        result = _is_local_extreme(data_set=np.array([1, 2, 3, 2, 5, 4, 3, 1, 2]),
+                                   current_index=7,
+                                   time_radius=1,
+                                   is_top=False)
+        assert result is True
 
     def test__is_data_point_a_local_swing__should_be_a_local_swing_bottom(self) -> None:
-        result = is_data_point_a_local_swing(data_set=np.array([1, 2, 3, 4, 1, 4, 3, 2, 1]),
+        data_set = np.array([4, 2, 3, 3, 5, 1, 3, 1, 2])
+
+        result_first_low = _is_local_extreme(data_set=data_set,
                                              current_index=5,
                                              time_radius=1,
                                              is_top=False)
-        assert result is False
+
+        result_second_low = _is_local_extreme(data_set=data_set,
+                                              current_index=7,
+                                              time_radius=1,
+                                              is_top=False)
+
+        assert result_first_low is True
+        assert result_second_low is True
+
+
+    def test__is_data_point_a_local_swing__should_not_be_a_2d_wide_local_swing_bottom(self) -> None:
+        data_set = np.array([4, 2, 3, 3, 5, 1, 3, 1, 2])
+
+        result_first_low = _is_local_extreme(data_set=data_set,
+                                             current_index=5,
+                                             time_radius=2,
+                                             is_top=False)
+
+        result_second_low = _is_local_extreme(data_set=data_set,
+                                              current_index=7,
+                                              time_radius=2,
+                                              is_top=False)
+
+        assert result_first_low is False
+        assert result_second_low is False
