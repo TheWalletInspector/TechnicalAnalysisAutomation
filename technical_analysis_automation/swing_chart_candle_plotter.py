a45424b316a1c94635f97f7fc4880500d296f0d4
Luke Taranto <luke.taranto@gmail.com>
2024-09-01 16:16:07 +1000

    add bar type detection

--- technical_analysis_automation/swing_chart_candle_plotter.py
+++ technical_analysis_automation/swing_chart_candle_plotter.py
@@ -5,6 +5,15 @@ from enum import Enum, auto
 import numpy as np
 import pandas as pd
 
+logging.basicConfig(level=logging.INFO)
+
+
+class BarType(Enum):
+    UP_BAR = auto()
+    DOWN_BAR = auto()
+    INSIDE_BAR = auto()
+    OUTSIDE_BAR = auto()
+
 
 @dataclass
 class Bar:
@@ -15,126 +24,102 @@ class Bar:
     open: float | None
     close: float | None
     volume: float | None
+    bar_type: BarType | None = None
 
 
-class bar_type(Enum):
-    up_bar = auto()
-    down_bar = auto()
-    inside_bar = auto()
-    outside_bar = auto()
-
-
-def _detect_bar_type(current_bar: Bar, previous_bar: Bar) -> bar_type:
+def _detect_bar_type(current_bar: Bar, previous_bar: Bar) -> BarType:
     if current_bar.high > previous_bar.high and current_bar.low > previous_bar.low:
-        return bar_type.up_bar
+        return BarType.UP_BAR
     elif current_bar.high < previous_bar.high and current_bar.low < previous_bar.low:
-        return bar_type.down_bar
+        return BarType.DOWN_BAR
     elif current_bar.high < previous_bar.high and current_bar.low > previous_bar.low:
-        return bar_type.inside_bar
+        return BarType.INSIDE_BAR
     elif current_bar.high > previous_bar.high and current_bar.low < previous_bar.low:
-        return bar_type.outside_bar
+        return BarType.OUTSIDE_BAR
     else:
         raise ValueError("Bar type not recognized")
 
 
-def _build_comparison_function(bar_type: bar_type, current_bar: Bar) -> np.ufunc:
-    if bar_type == bar_type.up_bar:
-        return np.max
-    elif bar_type == bar_type.down_bar:
-        return np.min
-    elif bar_type == bar_type.outisde_bar:
-        if current_bar.open > current_bar.close:
-            return np.max, np.min
-        else:
-            return np.min, np.max
+def _get_comparison_operators(bar_type: BarType, current_bar: Bar) -> tuple:
+    if bar_type == BarType.UP_BAR:
+        return (np.max,)
+    elif bar_type == BarType.DOWN_BAR:
+        return (np.min,)
+    elif bar_type == BarType.OUTSIDE_BAR:
+        return (np.max, np.min) if current_bar.open > current_bar.close else (np.min, np.max)
     else:
-        raise ValueError("Bar type not recognized")
+        raise ValueError(f"Bar type not recognized: {bar_type}")
 
 
-def _is_local_extreme(data_set: np.array, current_index: int, time_radius: int, comparison_operator) -> bool:
-    if current_index < time_radius:
+def _is_local_extreme(data_set_bars: list[Bar], current_index: int, time_radius: int, comparison_operator) -> bool:
+    if current_index < time_radius or current_index > len(data_set_bars) - time_radius:
         logging.info(f"current_index: {current_index} does not have enough data points to form a rolling window")
         return False
 
+    data_set_array = np.array([bar.high if comparison_operator == np.max else bar.low for bar in data_set_bars])
     start = max(0, current_index - time_radius)
-    end = min(len(data_set), current_index + time_radius + 1)
-    window = data_set[start:end]
+    end = min(len(data_set_array), current_index + time_radius + 1)
+    window = data_set_array[start:end]
 
-    is_extreme = data_set[current_index] == comparison_operator(window) and np.sum(
-        window == data_set[current_index]) == 1
+    is_extreme = data_set_array[current_index] == comparison_operator(window) and np.sum(
+        window == data_set_array[current_index]) == 1
 
-    print(f"current_index: {current_index}")
-    print(f"start: {start}")
-    print(f"end: {end}")
-    print(f"window: {window}")
-    print(f"is_extreme: {is_extreme}")
+    logging.debug(f"current_index: {current_index}")
+    logging.debug(f"start: {start}")
+    logging.debug(f"end: {end}")
+    logging.debug(f"window: {window}")
+    logging.debug(f"is_extreme: {is_extreme}")
 
     return bool(is_extreme)
 
 
-def detect_swing_extremes_across_data_set(data_set: pd.DataFrame, time_radius: int) -> pd.DataFrame:
-    """
-    Detects swing highs and lows across the data set
-    """
+def detect_swing_extremes_across_data_set(data_set_bars: list[Bar], time_radius: int) -> pd.DataFrame:  # noqa: D103
     swing_extremes = []
-    for index, row in data_set.iterrows():
-        current_bar = Bar(
-            index=int(index),  # Using the correct index
-            date=row.date,  # Assuming 'date' is the index after setting it
+    for index, current_bar in enumerate(data_set_bars):
+        previous_bar = data_set_bars[index - 1] if index > 0 else None
+
+        if previous_bar:
+            current_bar.bar_type = _detect_bar_type(current_bar=current_bar, previous_bar=previous_bar)
+            if current_bar.bar_type == BarType.INSIDE_BAR:
+                continue
+
+            comparison_operators = _get_comparison_operators(bar_type=current_bar.bar_type, current_bar=current_bar)
+
+            print(f"current_bar: {current_bar}")
+
+            for operator in comparison_operators:
+                if _is_local_extreme(data_set_bars, current_bar.index, time_radius, operator):
+                    swing_extremes.append(current_bar)
+
+    return pd.DataFrame([vars(extreme) for extreme in swing_extremes],
+                        columns=["index", "date", "high", "low", "open", "close", "volume", "bar_type"])
+
+
+def _parse_data_set(data: pd.DataFrame) -> list[Bar]:
+    data_set_bars = []
+    for index, row in data.iterrows():
+        data_set_bars.append(Bar(
+            index=int(index),
+            date=row.date,
             high=row.high,
             low=row.low,
             open=row.open,
             close=row.close,
-            volume=row.volume if 'volume' in row else None  # Handle volume if it exists
-        )
+            volume=row.volume if 'volume' in row else None
+        ))
 
-        # Detect swing high
-        comparison_operator = np.max if current_bar.open > current_bar.close else np.min
-        comparison_field = 'high' if current_bar.open > current_bar.close else 'low'
-        if _is_local_extreme(data_set.to_numpy(), current_bar.index, time_radius, comparison_operator):
-            swing_extremes.append(current_bar)
-
-        # Detect swing low
-        # comparison_operator = np.min if current_bar.open < current_bar.close else np.max
-        if _is_local_extreme(data_set.to_numpy(), current_bar.index, time_radius, comparison_operator):
-            swing_extremes.append(current_bar)
-
-    return pd.DataFrame([vars(extreme) for extreme in swing_extremes],
-                        columns=["index", "date", "high", "low", "open", "close", "volume"])
+    return data_set_bars
 
 
-def main() -> None:
-    """
-    Main function to detect swing extremes across the data set
-    """
-    # Load and prepare the data
+def main() -> None:  # noqa: D103
     data = pd.read_csv('.././data/BTCUSDT86400.csv')
     data['date'] = data['date'].astype('datetime64[s]')
-    # data = data.set_index('date')
 
-    # Detect swing extremes
-    swing_extremes = detect_swing_extremes_across_data_set(data, time_radius=2)
+    data_set_bars: list[Bar] = _parse_data_set(data)
+    swing_extremes = detect_swing_extremes_across_data_set(data_set_bars, time_radius=2)
 
-    print("Swing Extremes")
-    print(swing_extremes)
-
-    # Uncomment and adjust the following block to plot the results if needed
-    # import mplfinance as mpf
-    # fig, axlist = mpf.plot(
-    #     data,
-    #     type='candle',  # candlestick chart
-    #     style='yahoo',  # chart style
-    #     addplot=[
-    #         mpf.make_addplot(swing_extremes['high'], type='scatter', color='green', marker='^', markersize=100),
-    #         mpf.make_addplot(swing_extremes['low'], type='scatter', color='red', marker='v', markersize=100),
-    #     ],
-    #     volume=False,
-    #     figratio=(10, 6),  # figure size
-    #     title='BTCUSDT Trading Chart',
-    #     ylabel='Price',
-    #     ylabel_lower='Volume'
-    # )
-    # plt.show()
+    # logging.info("Swing Extremes")
+    # logging.info(swing_extremes)
 
 
 if __name__ == '__main__':
--- tests/test__swing_chart_candle_plotter.py
+++ tests/test__swing_chart_candle_plotter.py
@@ -2,92 +2,62 @@ import numpy as np
 import pandas as pd
 
 from technical_analysis_automation.swing_chart_candle_plotter import (
+    Bar,
+    BarType,
     _is_local_extreme,
+    _parse_data_set,
     detect_swing_extremes_across_data_set,
 )
 
 
 class TestSwingChartCandlePlotter:
     def test__is_data_point_a_local_swing__should_not_have_enough_data_points(self) -> None:
-        result = _is_local_extreme(data_set=np.array([1, 2, 3, 4, 5, 4, 3, 2, 1]),
-                                   current_index=1,
-                                   time_radius=2,
-                                   comparison_operator=np.max)
+        data_set = [Bar(index=i, date=None, high=val, low=val, open=None, close=None, volume=None) for i, val in
+                    enumerate([1, 2, 3, 4, 5, 4, 3, 2, 1])]
+        result = _is_local_extreme(data_set_bars=data_set, current_index=1, time_radius=2, comparison_operator=np.max)
         assert result is False
 
     def test__is_data_point_a_local_swing__should_be_a_local_swing_top(self) -> None:
-        result = _is_local_extreme(data_set=np.array([1, 2, 3, 4, 5, 4, 3, 2, 1]),
-                                   current_index=4,
-                                   time_radius=1,
-                                   comparison_operator=np.max)
+        data_set = [Bar(index=i, date=None, high=val, low=val, open=None, close=None, volume=None) for i, val in
+                    enumerate([1, 2, 3, 4, 5, 4, 3, 2, 1])]
+        result = _is_local_extreme(data_set_bars=data_set, current_index=4, time_radius=1, comparison_operator=np.max)
         assert result is True
 
     def test__is_data_point_a_local_swing__should_be_a_local_2d_wide_swing_top(self) -> None:
-        result = _is_local_extreme(data_set=np.array([1, 2, 3, 4, 5, 4, 3, 2, 1]),
-                                   current_index=4,
-                                   time_radius=2,
-                                   comparison_operator=np.max)
+        data_set = [Bar(index=i, date=None, high=val, low=val, open=None, close=None, volume=None) for i, val in
+                    enumerate([1, 2, 3, 4, 5, 4, 3, 2, 1])]
+        result = _is_local_extreme(data_set_bars=data_set, current_index=4, time_radius=2, comparison_operator=np.max)
         assert result is True
 
     def test__is_data_point_a_local_swing__should_not_be_a_local_2d_wide_swing_top(self) -> None:
-        data_set = np.array([1, 2, 5, 4, 5, 4, 3, 2, 1])
-
-        result_first_max = _is_local_extreme(data_set=data_set,
-                                             current_index=2,
-                                             time_radius=2,
+        data_set = [Bar(index=i, date=None, high=val, low=val, open=None, close=None, volume=None) for i, val in
+                    enumerate([1, 2, 5, 4, 5, 4, 3, 2, 1])]
+        result_first_max = _is_local_extreme(data_set_bars=data_set, current_index=2, time_radius=2,
                                              comparison_operator=np.max)
-
-        result_second_mox = _is_local_extreme(data_set=data_set,
-                                              current_index=4,
-                                              time_radius=2,
+        result_second_max = _is_local_extreme(data_set_bars=data_set, current_index=4, time_radius=2,
                                               comparison_operator=np.max)
-
         assert result_first_max is False
-        assert result_second_mox is False
+        assert result_second_max is False
 
     def test__is_data_point_a_local_swing__should_not_be_a_local_swing_top(self) -> None:
-        result = _is_local_extreme(data_set=np.array([1, 2, 5, 4, 5, 4, 3, 2, 1]),
-                                   current_index=4,
-                                   time_radius=2,
-                                   comparison_operator=np.max)
+        data_set = [Bar(index=i, date=None, high=val, low=val, open=None, close=None, volume=None) for i, val in
+                    enumerate([1, 2, 5, 4, 5, 4, 3, 2, 1])]
+        result = _is_local_extreme(data_set_bars=data_set, current_index=4, time_radius=2, comparison_operator=np.max)
         assert result is False
 
-    def test__is_data_point_a_local_swing__should_not_be_a_local_swing_bottom(self) -> None:
-        result = _is_local_extreme(data_set=np.array([1, 2, 3, 2, 5, 4, 3, 1, 2]),
-                                   current_index=7,
-                                   time_radius=1,
-                                   comparison_operator=np.min)
+    def test__is_data_point_a_local_swing__should_be_a_local_swing_bottom(self) -> None:
+        data_set = [Bar(index=i, date=None, high=val, low=val, open=None, close=None, volume=None) for i, val in
+                    enumerate([1, 2, 3, 2, 5, 4, 3, 1, 2])]
+        result = _is_local_extreme(data_set_bars=data_set, current_index=7, time_radius=1, comparison_operator=np.min)
         assert result is True
 
-    def test__is_data_point_a_local_swing__should_be_a_local_swing_bottom(self) -> None:
-        data_set = np.array([4, 2, 3, 3, 5, 1, 3, 1, 2])
-
-        result_first_low = _is_local_extreme(data_set=data_set,
-                                             current_index=5,
-                                             time_radius=1,
-                                             comparison_operator=np.min)
-
-        result_second_low = _is_local_extreme(data_set=data_set,
-                                              current_index=7,
-                                              time_radius=1,
-                                              comparison_operator=np.min)
-
-        assert result_first_low is True
-        assert result_second_low is True
-
     def test__is_data_point_a_local_swing__should_not_be_a_2d_wide_local_swing_bottom(self) -> None:
-        data_set = np.array([4, 2, 3, 3, 5, 1, 3, 1, 2])
-
-        result_first_low = _is_local_extreme(data_set=data_set,
-                                             current_index=5,
-                                             time_radius=2,
+        data_set = [Bar(index=i, date=None, high=val, low=val, open=None, close=None, volume=None) for i, val in
+                    enumerate([4, 2, 3, 3, 5, 1, 3, 1, 2])]
+        result_first_low = _is_local_extreme(data_set_bars=data_set, current_index=5, time_radius=2,
                                              comparison_operator=np.min)
-
-        result_second_low = _is_local_extreme(data_set=data_set,
-                                              current_index=7,
-                                              time_radius=2,
+        result_second_low = _is_local_extreme(data_set_bars=data_set, current_index=7, time_radius=2,
                                               comparison_operator=np.min)
-
         assert result_first_low is False
         assert result_second_low is False
 
@@ -96,17 +66,18 @@ class TestSwingChartCandlePlotter:
         data = {
             'date': pd.date_range(start='2023-01-01', periods=10, freq='D'),
             'high': [1, 2, 3, 4, 5, 4, 3, 2, 1, 2],
-            'low': [0, 1, 2, 3, 4, 3, 2, 1, 0, 1],
+            'low': [0, 1, 2, 3, 2, 3, 2, 1, 0, 1],
             'open': [0.5, 1.5, 2.5, 3.5, 4.5, 3.5, 2.5, 1.5, 0.5, 1.5],
             'close': [0.8, 1.8, 2.8, 3.8, 4.8, 3.8, 2.8, 1.8, 0.8, 1.8],
             'volume': [100, 200, 300, 400, 500, 400, 300, 200, 100, 200]
         }
         df = pd.DataFrame(data)
-        print(df)
+
+        # Parse the data set
+        data_set_bars = _parse_data_set(df)
 
         # Call the function
-        result = detect_swing_extremes_across_data_set(df, time_radius=2)
-        print(result)
+        result = detect_swing_extremes_across_data_set(data_set_bars, time_radius=2)
 
         # Expected swing extremes
         expected_data = {
@@ -119,9 +90,12 @@ class TestSwingChartCandlePlotter:
             'low': [4, 4, 0, 0],
             'open': [4.5, 4.5, 0.5, 0.5],
             'close': [4.8, 4.8, 0.8, 0.8],
-            'volume': [500, 500, 100, 100]
+            'volume': [500, 500, 100, 100],
+            'bar_type': [BarType.OUTSIDE_BAR, BarType.OUTSIDE_BAR, BarType.DOWN_BAR, BarType.DOWN_BAR]
         }
         expected_df = pd.DataFrame(expected_data)
+
+        print(result)
         print(expected_df)
 
         # Assert the results
